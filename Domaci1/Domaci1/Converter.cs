dce1025b771002e54889284b01de4a3d16163a19
DaraYo <dara94j@gmail.com>
2017-03-23 00:34:03 +0100

    Resenje prvog domaceg zadatka

---
+++ Domaci1/Domaci1/Converter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domaci1
+{
+    class Converter
+    {
+        //f = c *9/5+32
+        //c=(f-32)/(9/5)
+        private double degree;
+
+	    public double Degree
+	    {
+		    get { return degree;}
+		    private set { degree = value;}
+	    }
+
+        public Converter ()
+	    {
+                
+	    }
+
+        public void From_c_to_f()
+        {
+            string option="";
+            while (!option.Equals("q"))
+            {
+                try
+                {
+                    Console.WriteLine("Ako zelite da se vratite na glavni meni, unesite q");
+                    Console.Write("Stepeni u celzijusima>>");
+                    option = Console.ReadLine();
+                    degree = (Convert.ToDouble(option)*1.8) + 32;
+                    Console.WriteLine("Stepeni u farenhajtima iznose: " + degree);
+                }
+                catch
+                {
+                    if(!option.Equals("q"))
+                        Console.WriteLine("Pogresan unos, moracete ponovo!");
+                }
+            }
+            Console.Clear();
+        }
+
+        public void From_f_to_c()
+        {
+            string option = "";
+            while (!option.Equals("q"))
+            {
+                try
+                {
+                    Console.WriteLine("Ako zelite da se vratite na glavni meni, unesite q");
+                    Console.Write("Stepeni u farenhajtima>>");
+                    option = Console.ReadLine();
+                    degree = (Convert.ToDouble(option) - 32) / 1.8;
+                    Console.WriteLine("Stepeni u celzijusima iznose: " + degree);
+                }
+                catch (Exception e)
+                {
+                    if (!option.Equals("q"))
+                        Console.WriteLine("Pogresan unos, moracete ponovo!");
+                }
+            }
+            Console.Clear();
+        }
+    }
+}
---
+++ Domaci1/Domaci1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domaci1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter c = new Converter();
+            double option=100;
+            while (option!= 0) 
+            {
+                Console.WriteLine("Opcije:");
+                Console.WriteLine("1. Konverzija iz celzijusa u farenhajte");
+                Console.WriteLine("2. Konverzija iz farenhajta u celzijuse");
+                Console.WriteLine("0. Izlaz iz programa");
+                Console.Write("Unesite broj:");
+                try
+                {
+                    option = Convert.ToDouble(Console.ReadLine());
+                }
+                catch
+                {
+                    option = 200;
+                }
+                if (option == 1)
+                {
+                    Console.Clear();
+                    c.From_c_to_f();
+                }
+                else if (option == 2)
+                {
+                    Console.Clear();
+                    c.From_f_to_c();
+                }
+                else if(option!=0)
+                {
+                    Console.WriteLine("Pogresan unos, pokusajte ponovo!");
+                }
+
+            }
+            
+
+
+
+
+        }
+    }
+}
